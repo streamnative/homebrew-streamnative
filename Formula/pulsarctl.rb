a697b29613f6a46f2dec44785dd01cef9daa498c
Yong Zhang <zhangyong1025.zy@gmail.com>
2020-03-26 14:07:08 +0800

    Upgrade pulsarctl to 0.4.0 (#6)

--- Formula/pulsarctl.rb
+++ Formula/pulsarctl.rb
@@ -2,14 +2,23 @@ class Pulsarctl < Formula
   desc "CLI for Apache Pulsar written in golang"
   homepage "https://streamnative.io/"
   url "https://github.com/streamnative/pulsarctl.git",
-      :tag      => "v0.3.0",
-      :revision => "eda516d8290061dd8b6e8675ec5b32e67321ed38"
+      :tag      => "v0.4.0",
+      :revision => "d1c0c2853572e1b0a8e676ec135c81c1b9690eb6"
   sha256 "d0d6b55017cf349dd893248b0541c8026db9be5d5a7c5523d7bb91426c279fe3"
 
   depends_on "go" => :build
 
   def install
     system "go", "build", "-o", bin / "pulsarctl"
+    system "mkdir", "-p", share / "plugins"
+    system "cp", "-r", "plugins/", share / "plugins"
+
+    puts "In order to use this plugins, please add the plugin directory '#{share}/plugins' to the system PATH. " \
+             "You can do so by adding the following line to your bash profile."
+    puts ""
+    puts "export PATH=${PATH}:#{share}/plugins"
+    puts ""
+    puts "Happy Pulsaring!"
   end
 
   test do
