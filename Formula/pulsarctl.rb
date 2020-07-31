95affc4354a811f8b2ac55bd11b68502b2a8527a
Yong Zhang <zhangyong1025.zy@gmail.com>
2020-07-31 11:04:34 +0800

    Upgradle pulsarctl 0.5.0

--- Formula/pulsarctl.rb
+++ Formula/pulsarctl.rb
@@ -2,8 +2,8 @@ class Pulsarctl < Formula
   desc "CLI for Apache Pulsar written in golang"
   homepage "https://streamnative.io/"
   url "https://github.com/streamnative/pulsarctl.git",
-      :tag      => "v0.4.0",
-      :revision => "d1c0c2853572e1b0a8e676ec135c81c1b9690eb6"
+      :tag      => "v0.5.0",
+      :revision => "615bbc787af151c1f872fdc4c5e048ac562b9618"
   sha256 "d0d6b55017cf349dd893248b0541c8026db9be5d5a7c5523d7bb91426c279fe3"
 
   depends_on "go" => :build
