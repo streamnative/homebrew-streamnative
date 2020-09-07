4e5b2ca46c4f90ce260a07d68d29173cac971ac7
Yong Zhang <zhangyong1025.zy@gmail.com>
2020-09-07 10:47:19 +0800

    Update the pulsar download link

--- Formula/pulsar.rb
+++ Formula/pulsar.rb
@@ -1,7 +1,7 @@
 class Pulsar < Formula
   desc "Pulsar is a distributed pub-sub messaging platform with a very flexible messaging model and an intuitive client API."
   homepage "https://github.com/streamnative/pulsar"
-  url "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/pulsar-2.5.0/apache-pulsar-2.5.0-bin.tar.gz"
+  url "https://archive.apache.org/dist/pulsar/pulsar-2.5.0/apache-pulsar-2.5.0-bin.tar.gz"
   sha256 "721b12c6fa7d87cbf2763d2b7c3ffc0c5395d2cb1f3760e9ae33d6f8c650a064"
 
   def install
