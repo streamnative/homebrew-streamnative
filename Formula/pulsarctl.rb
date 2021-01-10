11bf8c7394d72896dd9b31571158fbf07633139b
Yong Zhang <zhangyong1025.zy@gmail.com>
2021-01-10 16:51:50 +0800

    Fix the ci issue

--- Formula/pulsarctl.rb
+++ Formula/pulsarctl.rb
@@ -2,6 +2,7 @@ class Pulsarctl < Formula
   desc "CLI for Apache Pulsar written in golang"
   homepage "https://streamnative.io/"
   url "https://github.com/streamnative/pulsarctl/releases/download/v2.7.0.2/pulsarctl-amd64-darwin.tar.gz"
+  sha256 "9c7b7519fa20e43a9e1bb614b5382273f8df35bed673c4092b351a9adeed1eb7"
 
   def install
     bin.install "pulsarctl"
