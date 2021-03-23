5cfb24153763e587b276be723c585e14913e7357
guangning <guangning@apache.org>
2021-03-23 14:50:38 +0800

    Update snctl version to latest

--- Formula/snctl.rb
+++ Formula/snctl.rb
@@ -1,8 +1,8 @@
 class Snctl < Formula
   desc "StreamNative cloud cli tools"
   homepage "https://streamnative.io/"
-  url "https://storage.googleapis.com/downloads.streamnative.cloud/snctl/v0.8.0/snctl_0.8.0_darwin_amd64.tar.gz"
-  sha256 "a9348a8138a45d8a9e73e489da054fc8d380bab1aff55ceb7cf404a4a86cca6c"
+  url "https://storage.googleapis.com/downloads.streamnative.cloud/snctl/v0.9.0/snctl_0.9.0_darwin_amd64.tar.gz"
+  sha256 "cdd2bfdf61a8c2a6ab29851f6407483c1f6dfaf41fb7b802cbf2cc424e8d0cb7"
 
   def install
     bin.install "snctl"
