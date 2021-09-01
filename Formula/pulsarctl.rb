7176a6941822cf9aa8e8c11cc1ede98da7998ded
Yufan Sheng <yufan@streamnative.io>
2021-09-02 07:53:42 +0800

    feat: support Linuxbrew for pulsarctl

--- Formula/pulsarctl.rb
+++ Formula/pulsarctl.rb
@@ -1,11 +1,22 @@
 class Pulsarctl < Formula
   desc "CLI for Apache Pulsar written in golang"
   homepage "https://streamnative.io/"
-  url "https://github.com/streamnative/pulsarctl/releases/download/v2.8.0.15/pulsarctl-amd64-darwin.tar.gz"
-  sha256 "2e05d525b012d713b9befab1a8c2cd680a53a3441df78a65fff8d2886ec5f443"
   license "Apache-2.0"
-
   bottle :unneeded
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/streamnative/pulsarctl/releases/download/v2.8.0.15/pulsarctl-amd64-darwin.tar.gz"
+      sha256 "2e05d525b012d713b9befab1a8c2cd680a53a3441df78a65fff8d2886ec5f443"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/streamnative/pulsarctl/releases/download/v2.8.0.15/pulsarctl-amd64-linux.tar.gz"
+      sha256 "c3e0df2b09ebaf2383dd502b10437473168d029c71fda897d7283759518289f7"
+    end
+  end
 
   def install
     libexec.install Dir["*"]
