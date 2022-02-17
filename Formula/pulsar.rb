6237214def7a10eaf298af315f6cf4cc9cde1bf3
Yufan Sheng <yufan@streamnative.io>
2022-02-18 01:32:15 +0800

    chore: bump pulsar to 2.9.1 fixed #29 (#31)

--- Formula/pulsar.rb
+++ Formula/pulsar.rb
@@ -1,8 +1,8 @@
 class Pulsar < Formula
   desc "Distributed pub-sub messaging platform with a very flexible messaging model"
   homepage "https://pulsar.apache.org"
-  url "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/pulsar-2.8.1/apache-pulsar-2.8.1-bin.tar.gz"
-  sha256 "aafd4a835ff100bde3df37220bfb950ce4dfb3c68f74680ba6c3bdd1cc3904cb"
+  url "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/pulsar-2.9.1/apache-pulsar-2.9.1-bin.tar.gz"
+  sha256 "485833c8459548188e4c4c106476ee02628bf7d88438ec8ccfaa85e56691f7ca"
   license "Apache-2.0"
 
   depends_on "openjdk" => :optional
