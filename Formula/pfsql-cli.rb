8cb768db01047b3ba96294c56e4add5bf67ff178
Rui Fu <rfu@apache.org>
2023-10-27 22:50:28 +0800

    bump pfsql-cli to 0.18.0

--- Formula/pfsql-cli.rb
+++ Formula/pfsql-cli.rb
@@ -1,23 +1,23 @@
 class PfsqlCli < Formula
   desc "StreamNative pfSQL CLI (pfsql-cli)"
   homepage "https://streamnative.io/"
-  version "0.17.0"
+  version "0.18.0"
   license "Apache-2.0"
 
   depends_on "curl"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://storage.googleapis.com/artifacts.affable-ray-226821.appspot.com/pfsql-cli/v0.17.0/pfsql-cli-aarch64-apple-darwin.tar.gz"
-      sha256 "a29d8c7b200caceaaf4de0ed921987d76d1afd06c04016dd7dbfbc25e34b9ab6"
+      url "https://storage.googleapis.com/artifacts.affable-ray-226821.appspot.com/pfsql-cli/v0.18.0/pfsql-cli-aarch64-apple-darwin.tar.gz"
+      sha256 "d06fafd0d213362ab2ac734bea29db8bf506894421210cad87ad4bb941e5e58a"
 
       def install
         bin.install "pfsql-cli-aarch64-apple-darwin" => "pfsql"
       end
     end
     if Hardware::CPU.intel?
-      url "https://storage.googleapis.com/artifacts.affable-ray-226821.appspot.com/pfsql-cli/v0.17.0/pfsql-cli-x86_64-apple-darwin.tar.gz"
-      sha256 "45cd48c05a046cde87b3fe11a45ef369548aa02236b57d4d273c79e53b6dd5f7"
+      url "https://storage.googleapis.com/artifacts.affable-ray-226821.appspot.com/pfsql-cli/v0.18.0/pfsql-cli-x86_64-apple-darwin.tar.gz"
+      sha256 "112d00ee606d6ad4d83a9cd0ea75fce4f384f24fc0b0a054f135fc15ecf21ec4"
 
       def install
         bin.install "pfsql-cli-x86_64-apple-darwin" => "pfsql"
@@ -27,16 +27,16 @@ class PfsqlCli < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://storage.googleapis.com/artifacts.affable-ray-226821.appspot.com/pfsql-cli/v0.17.0/pfsql-cli-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "19a13f0b5b469815cf5b67fe67a34eee97fc62f9492f587023d2e4f408d7e8a1"
+      url "https://storage.googleapis.com/artifacts.affable-ray-226821.appspot.com/pfsql-cli/v0.18.0/pfsql-cli-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "b27ad8f40daa8737742fa1cc708486afe69dda318824ca693981457ea3eb404d"
 
       def install
         bin.install "pfsql-cli-x86_64-unknown-linux-gnu" => "pfsql"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://storage.googleapis.com/artifacts.affable-ray-226821.appspot.com/pfsql-cli/v0.17.0/pfsql-cli-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "65a5835b9c315318b4f0a1c99611513906293c30871d9f1f2fa3f89649983ca7"
+      url "https://storage.googleapis.com/artifacts.affable-ray-226821.appspot.com/pfsql-cli/v0.18.0/pfsql-cli-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "d88d615f9effe555c5291bd86fe013224f1fda62cd6eaf62dd369a779a63246a"
 
       def install
         bin.install "pfsql-cli-aarch64-unknown-linux-gnu" => "pfsql"
