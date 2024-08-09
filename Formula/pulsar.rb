5afe9b5f22c170f47262b3d6531fb58c1d6c00de
ericsyh <ericshenyuhao@outlook.com>
2024-08-09 13:31:44 +0800

    fix the ruby style
    Signed-off-by: ericsyh <ericshenyuhao@outlook.com>

--- Formula/pulsar.rb
+++ Formula/pulsar.rb
@@ -10,7 +10,7 @@ class Pulsar < Formula
   def install
     libexec.install Dir["bin", "lib", "instances", "conf"]
 
-    (libexec/"lib/presto/bin/procname/Linux-ppc64le").rmtree
+    rm_r(libexec/"lib/presto/bin/procname/Linux-ppc64le")
     pkgshare.install Dir["examples", "licenses"]
     (etc/"pulsar").install_symlink libexec/"conf"
 
